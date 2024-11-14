8ec39348c4b2d7ae9f71144f23b3300a893f3d7b
Raphael Pinheiro <70273577+raphael-ps@users.noreply.github.com>
2024-11-14 14:28:15 -0300

    fix(errors): Create exception handler for duplicate entries

---
+++ src/main/java/io/github/raphael/url_shortener/infra/RequestExceptionHandler.java
@@ -0,0 +1,23 @@
+package io.github.raphael.url_shortener.infra;
+
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RequestExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Object> hanldeDataIntegrityViolantion(DataIntegrityViolationException exception){
+        String message = "An error occurred while processing the request.\n";
+
+        if (exception.getCause() instanceof ConstraintViolationException) {
+            message += "Nickname already exists. Please choose another nickname.";
+        }
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(message);
+    }
+}
